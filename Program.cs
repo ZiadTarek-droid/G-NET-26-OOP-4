0dee03e560d207f05925b8b0ea600c470b272dcb
zeyad tarek <vetogamer5@outlook.com>
2026-03-01 05:16:01 +0200

    Q1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
-﻿namespace ConsoleApp1
+﻿using System.Numerics;
+using System.Text;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            #region Q1
+        //    Static Binding(Early Binding)
+
+        //    Happens at compile time.
+            
+        //    The method call is resolved before the program runs.
+            
+        //    Faster performance.
+            
+        //    Used with:
+            
+        //    Method Overloading
+            
+        //    Static methods
+            
+        //    Non -virtual methods
+        //    class Test
+        //{
+        //    public void Show()
+        //    {
+        //        Console.WriteLine("Static Binding");
+        //    }
+        //}
+        #endregion
+    }
     }
 }
